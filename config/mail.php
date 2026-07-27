daa734ddf2675dfef705633cac4c3be7d9f09cc5
Nguyễn Anh Tú <tunguyenanh105182.hnue@gmail.com>
2026-07-27 17:38:41 +0700

    update

---
+++ config/mail.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+/* Local SMTP settings. This file is ignored by Git. */
+define('SMTP_HOST', 'smtp.gmail.com');
+define('SMTP_PORT', 587);
+define('SMTP_USERNAME', 'tunguyenanh2122004@gmail.com');
+define('SMTP_PASSWORD', 'vraw orot wkas gjiu');
+define('SMTP_ENCRYPTION', 'tls');
+define('SMTP_FROM_EMAIL', 'tunguyenanh2122004@gmail.com');
+define('SMTP_FROM_NAME', 'He thong dang ky de tai');
